ea3cfe5a3c1b8d645a8395bb5b41211ce2f58359
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-03-07 13:58:29 -0700

    BATMAN
    adds initial lazyvim config

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+    event = "BufWinEnter",
+    -- config = function()
+    --   require("vim-fugitive").setup()
+    -- end,
+  },
+}
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    -- NOTE: still need to figure out why hidden files are dimmed, and why /custom is considered hidden
+    config = function(_, opts)
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        },
+      })
+    end,
+  },
+}
