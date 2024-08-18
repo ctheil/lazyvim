eb7daa6dc4ca574e84ecf1cbf8a903bcc83e6e2f
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-08-18 09:06:48 -0600

    RM: neotree and map leader-pv to explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,4 @@
 --
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,15 +10,18 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim",                                import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
-    -- { import = "lazyvim.plugins.extras.lang.pug" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
